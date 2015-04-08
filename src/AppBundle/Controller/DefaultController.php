6f5c54dc7c0ac0c0e47b6ae9e1efad73a4e325b7
Mario Rezende <mariorez@gmail.com>
2015-04-08 08:29:06 -0300

    create product page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/product", name="product")
+     */
+    public function productAction()
+    {
+        return $this->render('default/product.html.twig');
     }
 }
