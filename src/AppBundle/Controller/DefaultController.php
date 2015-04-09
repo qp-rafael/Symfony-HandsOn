db15dfd631f768cd08e45501869115d9c1895466
Mario Rezende <mariorez@gmail.com>
2015-04-09 07:51:37 -0300

    fetching data from DB

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,14 +12,34 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $products = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->findAll();
+
+        if (empty($products)) {
+            throw $this->createNotFoundException('No products avaiable');
+        }
+
+        return $this->render('default/index.html.twig',
+            array('products' => $products)
+        );
     }
 
-    /**
-     * @Route("/product", name="product")
-     */
-    public function productAction()
-    {
-        return $this->render('default/product.html.twig');
+/**
+ * @Route("/product/{id}", name="product", requirements={"id": "\d+"})
+ */
+public function productAction($id)
+{
+    $product = $this->getDoctrine()
+        ->getRepository('AppBundle:Product')
+        ->findOneById($id);
+
+    if (!$product) {
+        throw $this->createNotFoundException('No product found with id: ' . $id);
     }
+
+    return $this->render('default/product.html.twig',
+        array('product' => $product)
+    );
+}
 }
