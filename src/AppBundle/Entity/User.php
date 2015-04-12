6b5a54afbeb3873abed2bfb9a2089ece45783201
Mario Rezende <mariorez@gmail.com>
2015-04-12 00:48:31 -0300

    fix code smells

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,10 +2,10 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
-use Doctrine\ORM\Mapping as ORM;
 
 /**
  * User
