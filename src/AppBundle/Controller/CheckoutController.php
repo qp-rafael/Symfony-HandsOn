fad9f9fe773b002b5699a40a79f873d562d9e76c
Mario Rezende <mariorez@gmail.com>
2015-04-12 02:39:51 -0300

    sending email

--- src/AppBundle/Controller/CheckoutController.php
+++ src/AppBundle/Controller/CheckoutController.php
@@ -4,14 +4,51 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class CheckoutController extends Controller
 {
     /**
      * @Route("/checkout", name="checkout")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $session = $request->getSession();
+        $cart = $session->get('cart');
+        if (!$cart || $cart->count() == 0) {
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('checkout/index.html.twig');
+    }
+
+    /**
+     * @Route("/success", name="success")
+     */
+    public function successAction(Request $request){
+        $session = $request->getSession();
+        $cart = $session->get('cart');
+        if (!$cart || $cart->count() == 0) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $user = $this->getUser();
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Merci Commerce: Pedido realizado com sucesso!')
+            ->setFrom('atendimento@merci.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'checkout/email.txt.twig',
+                    array('user' => $user, 'cart' => $cart)
+                ));
+
+        $this->get('mailer')->send($message);
+
+        $session->remove('cart');
+
+        return $this->render('checkout/success.html.twig');
+
     }
 }
